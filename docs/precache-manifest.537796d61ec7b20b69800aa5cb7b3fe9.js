73041ef98e64e6107bae2585f59e64f256da27f6
EricRCorral <eric-wil@hotmail.com>
2020-06-19 21:13:22 -0300

    Folder renamed

---
+++ docs/precache-manifest.537796d61ec7b20b69800aa5cb7b3fe9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e33bc11c23e9dae996833d3a11ca371",
+    "url": "/Chiches/index.html"
+  },
+  {
+    "revision": "5da81cd12fa4f79d4c77",
+    "url": "/Chiches/static/css/main.205f021d.chunk.css"
+  },
+  {
+    "revision": "9eb4d730c2e71876483a",
+    "url": "/Chiches/static/js/2.1f06f050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chiches/static/js/2.1f06f050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da81cd12fa4f79d4c77",
+    "url": "/Chiches/static/js/main.a762f313.chunk.js"
+  },
+  {
+    "revision": "2be4c4877386d7d8af75",
+    "url": "/Chiches/static/js/runtime-main.cb2dbf0d.js"
+  },
+  {
+    "revision": "86ee387760973a04c09be9b0392479f9",
+    "url": "/Chiches/static/media/c3-1.86ee3877.wav"
+  },
+  {
+    "revision": "b9ec5afaa555bc565713dd538997b972",
+    "url": "/Chiches/static/media/c3-2.b9ec5afa.wav"
+  },
+  {
+    "revision": "c096460edfc38f876d2c9dd4a2d37af7",
+    "url": "/Chiches/static/media/c3-3.c096460e.wav"
+  },
+  {
+    "revision": "9631cd0e2084da1467bdd53d1f03baa7",
+    "url": "/Chiches/static/media/c3-4.9631cd0e.wav"
+  },
+  {
+    "revision": "4af555edff6ebd211d6f74c1b8c66d50",
+    "url": "/Chiches/static/media/c3-5.4af555ed.wav"
+  },
+  {
+    "revision": "21edf29776762bb2299dfa72bc5d9b29",
+    "url": "/Chiches/static/media/c3-6.21edf297.wav"
+  },
+  {
+    "revision": "1ce6b15a4cb6e49d18becc9437b37c01",
+    "url": "/Chiches/static/media/c3-7.1ce6b15a.wav"
+  },
+  {
+    "revision": "bc82c5ed6fdfce561d296382c6e7fcb5",
+    "url": "/Chiches/static/media/c3-8.bc82c5ed.wav"
+  }
+]);
